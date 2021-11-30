a3d2cb16d188e1589ea2c6d904838be95637d198
julieher <92413796+julieher@users.noreply.github.com>
2021-11-30 17:17:24 -0600

    Adding schema and queries files

--- schema.sql
+++ schema.sql
@@ -17,14 +17,14 @@ CREATE TABLE employees(
 	PRIMARY KEY(emp_no)
 );
 
-CREATE TABLE dept_manager(
+CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES employees (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    	PRIMARY KEY (emp_no, dept_no) --Indent question? not running---
 );
 
 CREATE TABLE salaries(
@@ -43,7 +43,7 @@ CREATE TABLE  dept_emp(
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, dept_no)
+		PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE title(
