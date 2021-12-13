759dfee6e4348fcea18d70fcd3692f29dde55143
julieher <92413796+julieher@users.noreply.github.com>
2021-12-12 23:05:32 -0600

    adding README

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -90,6 +90,8 @@ FROM retirement_info
 LEFT JOIN dept_emp
 ON retirement_info.emp_no = dept_emp.emp_no;
 
+SELECT * FROM retirement_info;
+
 -- Alias on the above JOIN table
 -- (ri)retirement_info, (de)dept_emp
 SELECT ri.emp_no,
@@ -168,13 +170,15 @@ SELECT  ce.emp_no,
 		ce.first_name,
 		ce.last_name,
 		d.dept_name
---INTO dept_info
+INTO dept_info
 FROM current_emp as ce
 	INNER JOIN dept_emp as de
 	ON (ce.emp_no = de.emp_no)
 	
 	INNER JOIN departments as d
 	ON (de.dept_no = d.dept_no);
+	
+SELECT * FROM dept_info;
 
 -- TAILORED LIST
 SELECT  ce.emp_no,
@@ -194,10 +198,79 @@ SELECT  ce.emp_no,
 		ce.first_name,
 		ce.last_name,
 		d.dept_name
---INTO sales_dev_retirement_info
+INTO sales_dev_retirement_info
 FROM current_emp as ce
 	INNER JOIN dept_emp as de
 	ON (ce.emp_no = de.emp_no)
 	INNER JOIN departments as d
 	ON (de.dept_no = d.dept_no)
 WHERE de.dept_no in ('d007','d005');
+
+SELECT * FROM sales_dev_retirement_info;
+
+------------------------------------------------------------------------------------------------------------------------------------
+
+-- DELIVERABLE 1: The Number of Retiring Employees by Title
+-- Export as retirement_titles.csv
+SELECT  e.emp_no, 
+		e.first_name, 
+		e.last_name,
+    	title.title,
+    	title.from_date,
+    	title.to_date
+INTO retirement_titles
+FROM employees as e
+    INNER JOIN title
+    ON (e.emp_no = title.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+-- Export as unique_titles.csv
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+					rt.first_name,
+					rt.last_name,
+					rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT COUNT(*)
+FROM unique_titles;
+
+-- retrieve the number of employees by their most recent job title who are about to retire
+SELECT title, COUNT(title) as count
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
+
+------------------------------------------------------------------------------------------------------------------------------------
+-- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+		e.first_name, 
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+	INNER JOIN dept_emp as de
+    ON (e.emp_no = de.emp_no)
+	INNER JOIN title as ti
+	ON (e.emp_no = ti.emp_no)
+  	WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty;
+
+SELECT COUNT(*)
+FROM mentorship_eligibilty;
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Pewlett-Hackard-Analysis
-Module 7
+
+## <u>Project Overview</u>
+
+The project consisted in analyzing employee data from Pewlett-Hackard in order to determine the number of retiring employees per title and identify employees who are eligible to participate in a mentorship program. With the analysis, the HR team is going to be able prepare for the "silver tsunami" and plan new mentorship programs accordingly. 
+
+## <u>Results</u>
+
+- After running several queries, the "Retirement Titles" was created in order to show employees eligible for retirement. The table was filtered to only show employees who were born between 1952 and 1955.  
+
+- From the "Retirement Titles" table, it was observed that some employees held various positions during their career in PH, causing duplicate entries for those employees. Therefore, a new table " Unique Titles" was created in order to show the most current and unique titles.
+
+
+    Retirement titles
+![retirement_titles.](/Resources/retirement_titles.png)
+
+    Unique titles
+![retirement_titles.](/Resources/unique_titles.png)
+
+
+- A table containing all retiring employees by title was made, in order to determine which titles will require new hires. From such table, the title "Senior Engineers" has the highest number of people retiring. Also, there are two leadership positions "Technique Leader" and "Manager" that probably will require a more extensive mentorship program. 
+
+    Retiring by title:
+
+    ![retiring_titles](/Resources/retiring_titles.png)
+
+
+- Since there will be seven titles available and the majority of them from engineering areas, the HR department should consider implementing a rotational program for new and internal hires. This will allow employees to get cross functional experience from different organizations.
+
+
+
+## <u>Summary</u>
+- How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+
+  From "Unique Titles" table, it was calculated that a total number of 90,398 employees are close to retirement. 
+![count](Resources/count.png)
+
+- Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+
+    From the "Mentorship Eligibility" table, we get that only 1,549 are qualified for this program. This will not be sufficient to fulfill all the available positions. A good idea could be to invest in employee career development to encourage younger generations in order to retain more talent. 
+
+    ![mentorship](Resources/mentorship.png)
+
+## <u>Resources</u>
+
+[SQL Count](https://www.w3resource.com/sql/aggregate-functions/count-function.php)
